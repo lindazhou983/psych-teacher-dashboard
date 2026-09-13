3b9b2e819a1b246f5b73efe84844b5d29a6b4f53
lindazhou983 <lindazhou983@gmail.com>
2026-09-13 14:23:27 +0800

    Add dashboard config

---
+++ config.js
@@ -0,0 +1,5 @@
+window.DASHBOARD_CONFIG = {
+  apiUrl: "",
+  googleSheetUrl: "https://docs.google.com/spreadsheets/d/1MOfg1CpTSydQy0DEAWryEKms7qFipElj0A7VrSb7Om8/edit",
+  consultTemplateUrl: "https://docs.google.com/document/d/1h1da6YAq6hKGpH3pIJ_WlCqkJC9TJDlRcR9pvN4FVsk/edit"
+};
