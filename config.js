ca3c29fbc6dfee536030a4dc68c42c5a517b7ffa
lindazhou983 <lindazhou983@gmail.com>
2026-09-13 14:33:33 +0800

    Connect dashboard to Google Apps Script API

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.DASHBOARD_CONFIG = {
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbw9-YDgF9oPTxPhdAvPbWFqnLtkIaD26HJ7sj2TEZAg5YjDeNR6zu0mXusImYiITDt4qw/exec",
   googleSheetUrl: "https://docs.google.com/spreadsheets/d/1MOfg1CpTSydQy0DEAWryEKms7qFipElj0A7VrSb7Om8/edit",
   consultTemplateUrl: "https://docs.google.com/document/d/1h1da6YAq6hKGpH3pIJ_WlCqkJC9TJDlRcR9pvN4FVsk/edit"
 };
